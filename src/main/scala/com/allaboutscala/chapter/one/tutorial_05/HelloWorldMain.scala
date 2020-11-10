7601278acc63009333e838974bcd3190277eab5a
Justin Smallwood <justin.d.smallwood@gmail.com>
2020-11-10 16:03:01 +1100

    Initial commit

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_05/HelloWorldMain.scala
@@ -0,0 +1,9 @@
+package com.allaboutscala.chapter.one.tutorial_05
+
+object HelloWorldMain {
+  // Unit is the equivalent of void or None. The main method is required as we have not extended App.
+  def main(args: Array[String]): Unit = {
+    println("Hello World from the main function!!")
+  }
+
+}
